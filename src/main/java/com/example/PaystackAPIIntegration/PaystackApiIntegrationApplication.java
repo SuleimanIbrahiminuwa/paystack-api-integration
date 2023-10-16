31694e15ff7869fa585c3453bebf7d2cc1d785da
saban <isuleimanibrahim@gmail.com>
2023-10-16 19:05:26 +0100

    added a boilder plate code

---
+++ src/main/java/com/example/PaystackAPIIntegration/PaystackApiIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.example.PaystackAPIIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystackApiIntegrationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PaystackApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PaystackAPIIntegration/PaystackApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaystackAPIIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystackApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
